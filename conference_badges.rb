bffca337af0df862a68130de972fcffa07eec5c1
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-24 21:38:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,7 @@ def assign_rooms(array)
 n_array= []
 counter= 0 
   array.each do (n_array.push("Hello, #{name} You'll be assigned to room #{counter}!"))
-counter += 1
+counter =+ 1
 end
 return n_array
 end
