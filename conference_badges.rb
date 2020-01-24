f64916352ca0851a97ca19cec5ca32be4b9d5188
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-24 21:40:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,7 @@ end
 def assign_rooms(array)
 
 n_array= []
-counter= 0 
+counter= 1 
   array.each do |name|
     n_array.push("Hello, #{name} You'll be assigned to room #{counter}!")
 counter =+ 1
